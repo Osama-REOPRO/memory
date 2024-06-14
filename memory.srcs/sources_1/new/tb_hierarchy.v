ad746ea634b45fc686d9040ceb9b36a442a48d3b
osama wsl <osama.hamada.work@gmail.com>
2024-06-14 11:43:18 +0300

    in progress write physical mem

--- memory.srcs/sources_1/new/tb_hierarchy.v
+++ memory.srcs/sources_1/new/tb_hierarchy.v
@@ -3,45 +3,61 @@
 
 module tb_hierarchy;
 
-localparam L1_C = 8;   		 // capacity (total words)
-localparam L2_C = L1_C*2;
-localparam L1_b = 2;   // block size (words per block)
-localparam L2_b = L1_b*2;
-localparam L1_N = 1;   // degree of associativity (blocks per set)
-localparam L2_N = L1_N*2;
+localparam L1_C  = 8;   	// capacity (total words)
+localparam L2_C  = L1_C*2;
+localparam Phy_C = L2_C*2;
+localparam Vir_C = Phy_C*2;
+
+localparam L1_b  = 2;   	// block size (words per block)
+localparam L2_b  = L1_b*2;
+localparam Phy_b = L2_b*2;
+localparam Vir_b = Phy_b*2;
+
+localparam L1_N  = 1;   	// degree of associativity (blocks per set)
+localparam L2_N  = L1_N*2;
+localparam Phy_N = L2_N*2;
+localparam Vir_N = Vir_b; // fully associative
 
 
-reg  			  			clk, rst;
+reg  			  clk, rst;
 
-reg [`op_N:0] 			op						 [1:2];
+reg [`op_N:0] op						 [1:4];
 
-reg  [31:0]   			address 				 [1:2];
+reg  [31:0]   address 				 [1:4];
 
-reg 			  			set_valid 			 [1:2];
-reg			  			set_tag 				 [1:2];
-reg 			  			set_dirty 			 [1:2];
-reg 			  			set_use 				 [1:2];
+reg 			  set_valid 			 [1:4];
+reg			  set_tag 				 [1:4];
+reg 			  set_dirty 			 [1:4];
+reg 			  set_use 				 [1:4];
 
-reg  			  			mem_operation 		 [1:2];
+reg  			  mem_operation 		 [1:4];
 
-wire 			  			hit_occurred  		 [1:2];
-wire 			  			empty_found	 		 [1:2];
-wire 			  			clean_found	 		 [1:2];
+wire 			  hit_occurred  		 [1:4];
+wire 			  empty_found	 		 [1:4];
+wire 			  clean_found	 		 [1:4];
 
-reg  [(4*L1_b)-1:0] 	valid_bytes_L1;
-reg  [(4*L2_b)-1:0] 	valid_bytes_L2;
+reg  [(4*L1_b)-1:0] 	 valid_bytes_L1;
+reg  [(4*L2_b)-1:0] 	 valid_bytes_L2;
+reg  [(4*Phy_b)-1:0]	 valid_bytes_phy;
+reg  [(4*Vir_b)-1:0]	 valid_bytes_vir;
 
-reg  [(32*L1_b)-1:0]	write_data_L1;
-reg  [(32*L2_b)-1:0]	write_data_L2;
+reg  [(32*L1_b)-1:0]	 write_data_L1;
+reg  [(32*L2_b)-1:0]	 write_data_L2;
+reg  [(32*Phy_b)-1:0] write_data_phy;
+reg  [(32*Vir_b)-1:0] write_data_vir;
 
-wire [(32*L1_b)-1:0]	read_data_L1;
-wire [(32*L2_b)-1:0]	read_data_L2;
+wire [(32*L1_b)-1:0]	 read_data_L1;
+wire [(32*L2_b)-1:0]	 read_data_L2;
+wire [(32*Phy_b)-1:0] read_data_phy;
+wire [(32*Vir_b)-1:0] read_data_vir;
 
 
-reg [(32*L1_b)-1:0]	val_evac_L1;
-reg [(32*L2_b)-1:0]	val_evac_L2;
+reg [(32*L1_b)-1:0]	 val_evac_L1;
+reg [(32*L2_b)-1:0]	 val_evac_L2;
+reg [(32*Phy_b)-1:0]	 val_evac_phy;
+reg [(32*Vir_b)-1:0]	 val_evac_vir;
 
-wire 						mem_operation_done [1:2];
+wire 						mem_operation_done [1:4];
 
 always #0.1 clk <= !clk; // clock runs at 10 MHz
 
@@ -50,23 +66,38 @@ initial begin
 	             
 		op[1],
 		op[2],
+		op[3],
+		op[4],
 					 
 		address[1],
 		address[2],
+		address[3],
+		address[4],
 		             
 		set_dirty[1],
 		set_dirty[2],
+		set_dirty[3],
+		set_dirty[4],
+
 		set_use[1],
 		set_use[2],
+		set_use[3],
+		set_use[4],
 		             
 		mem_operation[1],
 		mem_operation[2],
+		mem_operation[3],
+		mem_operation[4],
 		             		
 		valid_bytes_L1,
 		valid_bytes_L2,
+		valid_bytes_phy,
+		valid_bytes_vir,
 		                  
 		write_data_L1,
-		write_data_L2 } = 0;
+		write_data_L2,
+		write_data_phy,
+		write_data_vir } = 0;
 
 
 	rst = 1;
@@ -74,7 +105,7 @@ initial begin
 	rst = 0;
 end
 
-reg [7:0] valToWrite [1:2];
+reg [7:0] valToWrite [1:4];
 
 wire [($clog2(L2_b) > 0 ? $clog2(L2_b)-1 : 0) :0] block_offset_in_adrs_L2 = 
 		(L2_b <= 1) ? 1'b0 : 
@@ -85,10 +116,11 @@ localparam w_lookup_st 	 		= 0,
 			  w_st 		  		 	= 1,
 			  w_fill_empty_w_st  = 2,
 			  w_evac_r_st  		= 3,
-			  w_h_evac_lookup_st = 4,
-			  w_h_evac_w_st		= 5,
-			  w_h_evac_fill_empty_w_st = 6,
-			  w_h_evac_r_st				= 7,			  
+			  w_evac_h_lookup_st = 4,
+			  w_evac_h_w_st		= 5,
+			  w_evac_h_fill_empty_w_st = 6,
+			  w_evac_h_evac_r_st			= 7,
+
 			  r_lookup_st		 	= 8,
 			  r_st 		  		 	= 9,
 			  r_fill_empty_w_st	= 10,
@@ -104,7 +136,7 @@ always @(posedge clk) begin : block_0
 		state 	 <= 0;
 		sub_state <= 0;
 
-		for (il=1; il<=2; il=il+1) begin
+		for (il=1; il<=4; il=il+1) begin
 			mem_operation [il] <= 1'b0;
 			address       [il] <= 1'b0;
 			write_data_L1 [il] <= 1'b0;
@@ -227,7 +259,7 @@ always @(posedge clk) begin : block_0
 					finish: begin
 						if (!mem_operation_done[1]) begin
 							val_evac_L1 <= read_data_L1;
-							state		  <= w_h_evac_lookup_st;
+							state		  <= w_evac_h_lookup_st;
 							sub_state  <= init;
 						end
 					end
@@ -235,7 +267,7 @@ always @(posedge clk) begin : block_0
 			end
 
 			// lookup higher cache for space to write evacuated data to
-			w_h_evac_lookup_st: begin
+			w_evac_h_lookup_st: begin
 				case (sub_state)
 					init: begin
 						op[2]			  <= `lookup_op;
@@ -252,9 +284,9 @@ always @(posedge clk) begin : block_0
 					end
 					finish: begin
 						if (!mem_operation_done[2]) begin
-							if 	  (hit_occurred[2]) 					  		state <= w_h_evac_w_st;		// write right away
-							else if (empty_found[2] || clean_found[2])  	state <= w_h_evac_fill_empty_w_st; 	// fill with zeroes then write
-							else 											  			state <= w_h_evac_r_st; // higher also needs an evac, so read value
+							if 	  (hit_occurred[2]) 					  		state <= w_evac_h_w_st;		// write right away
+							else if (empty_found[2] || clean_found[2])  	state <= w_evac_h_fill_empty_w_st; 	// fill with zeroes then write
+							else 											  			state <= w_evac_h_evac_r_st; // higher also needs an evac, so read value
 
 							sub_state <= init;
 						end
@@ -263,7 +295,7 @@ always @(posedge clk) begin : block_0
 			end
 
 			// write the evacuated data to higher level
-			w_h_evac_w_st: begin
+			w_evac_h_w_st: begin
 				case (sub_state)
 					init: begin
 						// done: this assignment makes no sense the two sizes are different
@@ -294,7 +326,7 @@ always @(posedge clk) begin : block_0
 				endcase
 			end
 
-			w_h_evac_fill_empty_w_st: begin
+			w_evac_h_fill_empty_w_st: begin
 				case (sub_state)
 					init: begin
 						write_data_L2 		  <= {(32*L2_b){1'b0}};
@@ -316,14 +348,14 @@ always @(posedge clk) begin : block_0
 					end
 					finish: begin
 						if (!mem_operation_done[2]) begin
-							state		 <= w_h_evac_w_st;
+							state		 <= w_evac_h_w_st;
 							sub_state <= init;
 						end
 					end
 				endcase
 			end
 
-			w_h_evac_r_st: begin
+			w_evac_h_evac_r_st: begin
 				case (sub_state)
 					init: begin
 						op[2] 			  <= `read_op;
@@ -342,16 +374,54 @@ always @(posedge clk) begin : block_0
 					finish: begin
 						if (!mem_operation_done[2]) begin
 							val_evac_L2 <= read_data_L2;
-							// for now we will do nothing with the data and just move
-							// on to filling with zeroes, in the future this data
-							// should be written to physical memory
-							state		  	<= w_h_evac_fill_empty_w_st;
+							// write data to higher levels
+							state		  	<= w_evac_h_evac_phy_lookup_st;
 							sub_state  	<= init;
 						end
 					end
 				endcase
 			end
 
+			w_evac_h_evac_phy_lookup_st: begin
+				// check if there is space to write the data evacuated from below
+				case (sub_state)
+					init: begin
+						op[3]			  <= `lookup_op;
+						mem_operation[3] <= 1'b1;
+
+						sub_state 	  <= busy;
+					end
+					busy: begin
+						if (mem_operation_done[3]) begin
+							mem_operation[3] <= 1'b0;
+
+							sub_state 	  <= finish;
+						end
+					end
+					finish: begin
+						if (!mem_operation_done[3]) begin
+							if 	  (hit_occurred[3]) 					  		state <= w_evac_h_evac_phy_w_st;		// write right away
+							else if (empty_found[3] || clean_found[3])  	state <= w_evac_h_evac_phy_fill_empty_w_st; 	// fill with zeroes then write
+							else 											  			state <= w_evac_h_evac_phy_evac_r_st; // physical also needs an evac, so read value
+
+							sub_state <= init;
+						end
+					end
+				endcase
+
+			end
+
+			w_evac_h_evac_phy_w_st: begin
+				// todo: write evacuated data
+			end
+			w_evac_h_evac_phy_fill_empty_w_st: begin
+				// todo: fill empty before write
+			end
+			w_evac_h_evac_phy_evac_r_st: begin
+				// todo: yet another evacuation
+			end
+
+			/////////////////////// read phase
 			r_lookup_st: begin
 				case (sub_state)
 					init: begin
@@ -545,6 +615,39 @@ l2_cache
 	.o_mem_operation_done(mem_operation_done[2])
 );
 
+cache 
+#(
+	.C(Phy_C), // capacity (words)
+	.b(Phy_b), // block size (words in block)
+	.N(Phy_N)  // degree of associativity
+) 
+physical_mem
+(
+	.i_clk(clk),
+	.i_rst(rst),
+
+	.i_op(op[3]),
+
+	.i_address(address[3]),
+
+	.i_set_valid(set_valid[3]),
+	.i_set_tag(set_tag[3]),
+	.i_set_dirty(set_dirty[3]),
+	.i_set_use(set_use[3]),
+
+	.i_mem_operation(mem_operation[3]),
+
+	.o_hit_occurred(hit_occurred[3]),
+	.o_empty_found(empty_found[3]),
+	.o_clean_found(clean_found[3]),
+
+	.i_valid_bytes(valid_bytes_phy),
+
+	.i_write_data(write_data_phy),
+	.o_read_data(read_data_phy),
+
+	.o_mem_operation_done(mem_operation_done[3])
+);
 // logs
 initial $display("\n\n(%0t) logs start //////////////////////////////////////////\n\n", $time);
 always @(state) begin
